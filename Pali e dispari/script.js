ad35b6c05689af857db25141f0fd3b23cc44df94
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-01-08 17:41:07 +0100

    optimise logic

--- Pali e dispari/script.js
+++ Pali e dispari/script.js
@@ -6,7 +6,7 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
 //creo il prompt per far scegliere pari o dispari all'utente
-let sceltaUtente = prompt("Scegli 'pari' o 'dispari'").toLocaleLowerCase();
+let sceltaUtente = prompt("Scegli 'pari' o 'dispari'");
 
 //creo il prompt per far inserire il numero all'utente
 let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
@@ -17,32 +17,29 @@ while (numeroUtente > 5) {
 
 //genero un numero random per il computer
 let numeroComputer = Math.floor(Math.random() * 5) + 1;
-
 console.log("Il numero del computer è: " + numeroComputer);
 
-//invoco e stampo il risultato della funzione
-console.log(pariDispari(sceltaUtente, numeroUtente, numeroComputer));
+//creo una variabile con il risultato della funzione
+let risultato = pariDispari(numeroUtente, numeroComputer);
 
+//logica per determinare chi ha vinto
+if (risultato == sceltaUtente) {
+    console.log("Hai vinto");
+}
+else {
+    console.log("Ha vinto il computer");
+}
 
 //creo la funzione con due argomenti
-function pariDispari(sceltaUtente, numeroUtente, numeroComputer) {
-    //creo la condizione per stabilire se la somma dei due numeri è pari o dispari
-    if ((numeroUtente + numeroComputer) % 2 === 0) {
-        console.log("la somma dei due numeri è pari");
-        if (sceltaUtente === "pari") {
-            return ("Ha vinto l'utente");
-        }
-        else {
-            return ("Ha vinto il computer");
-        }
+function pariDispari(primoNumero, secondoNumero) {
+
+    //creo la variabile booleana
+    let isEven = (primoNumero + secondoNumero) % 2 === 0;
+    //creo la condizione
+    if (isEven) {
+        return "pari";
     }
     else {
-        console.log("la somma dei due numeri è dispari");
-        if (sceltaUtente === "dispari") {
-            return ("Ha vinto l'utente");
-        }
-        else {
-            return ("Ha vinto il computer");
-        }
+        return "dispari";
     }
-    }
+}
--- Palidroma/script.js
+++ Palidroma/script.js
@@ -5,27 +5,30 @@ Creare una funzione per capire se la parola inserita è palindroma */
 //creo il prompt per l'utente
 let parolaUtente = prompt("Inserisci una parola");
 
-//invoco la funzione e stampo il risultato
-console.log(parolePalindrome());
+//creo una variabile con il risultato della funzione
+let risultato = parolePalindrome(parolaUtente);
+
+//logica per determinare se la parola è palindroma
+if (risultato) {
+    console.log("La parola è palindroma");
+}
+else {
+    console.log("La parola non è palindroma");
+}
 
 //creo la funzione
-function parolePalindrome() {
+function parolePalindrome(parolaDaTestare) {
     
     //creo un ciclo per invertire la parola inserita dall'utente
     let parolaInvertita = "";
-    for (let i = parolaUtente.length - 1; i >= 0; i--){
-        let carattereIesimo = parolaUtente[i];
+    for (let i = parolaDaTestare.length - 1; i >= 0; i--){
+        let carattereIesimo = parolaDaTestare[i];
         parolaInvertita += carattereIesimo;
     }
-
-    //creo la condizione per stabilire se la parola è palindroma
-    if (parolaUtente === parolaInvertita) {
-        return("La parola è palindroma");
-    }
-    else {
-        return("La parola non è palindroma");
-    }
+    return (parolaDaTestare === parolaInvertita); //Restituisce vero se la parola da testare è uguale alla parola invertita
 } 
+
+
 
 
 
